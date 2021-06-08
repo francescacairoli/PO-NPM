32238e8cdc6985e31386a0f9ff41aee417a2dc63
Francesca Cairoli <43812787+francescacairoli@users.noreply.github.com>
2021-06-08 11:26:39 +0200

    Delete utils directory

--- utils/predictDNNensbl.m
+++
@@ -1,9 +0,0 @@
-function T_pred = predictDNNensbl(DNNs,X_test)
-
-nsamples = size(X_test,2);
-T_pred = zeros(1,nsamples);
-for i=1:length(DNNs)
-    net = DNNs{i}.net;
-    T_pred = T_pred+round(net(X_test));
-end
-T_pred = round(T_pred./length(DNNs));
